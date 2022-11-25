7bd47d5750b91431f779d2c069f813386cbbbf5d
Charlie <2295509496@qq.com>
2022-11-25 12:38:50 +0800

    add finditer

--- chapter14/re模块.py
+++ chapter14/re模块.py
@@ -7,5 +7,14 @@ def main():
     res = pattern.findall(content)
     print(res)
 
+    """
+    re.finditer函数是re.findall函数的惰性版本，返回的不是列表，而是一个生成器，按需生成re.MatchObject实例。
+    如果有很多匹配，re.finditer函数能节省大量内存。
+    """
+
+    res2 = pattern.finditer(content)
+    print(res2)           # <class 'callable_iterator'>
+    print(next(res2).group())   # 'Hello'
+
 if __name__ == '__main__':
     main()  # ['Hello', 'from', 'to', 'you', 'to]
